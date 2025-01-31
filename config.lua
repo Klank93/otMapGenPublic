8d9d75863fd01bb9864c7346781a6ba2664f44e9
Abecek <abcq12013@gmail.com>
2025-01-31 01:31:17 +0100

    disabled logging and debug output on main branch

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 MAP_GEN_CFG = {
     generatorName = "mapGen",
     prefix = "[map_generator]: ", -- todo: not being used?
-    logToFile = true,
-    debugOutput = true
+    logToFile = false,
+    debugOutput = false
 }
